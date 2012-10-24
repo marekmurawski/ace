1b205f7aad1dc7c3725a6f0346bbd2ccd923b6a7
NicNLD <nic-nld@live.nl>
2012-10-24 19:54:38 +0200

    Adds translation template and dutch translation

---
+++ i18n/nl-message.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Wolf CMS - Content Management Simplified. <http://www.wolfcms.org>
+ * Copyright (C) 2008-2010 Martijn van der Kleijn <martijn.niji@gmail.com>
+ * 
+ * Ace filter for Wolf CMS
+ * Code editor and syntax highlighter based on Ajax.org Cloud9 Editor.
+ *  
+ * @package Plugins
+ * @subpackage ace
+ *
+ * @author Marek Murawski <http://marekmurawski.pl>
+ * @copyright Marek Murawski, 2012
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3 license
+ * @license Ace http://opensource.org/licenses/BSD-3-Clause BSD
+ * 
+ */
+
+/* Security measure */
+if (!defined('IN_CMS')) { exit(); }
+
+return array(
+    '1 Day' => '1 dag',
+    '1 Month' => '1 maand',
+    '1 Week' => '1 week',
+    '1 Year' => '1 jaar',
+    '10 Years' => '10 jaar',
+    '2 Days' => '2 dagen',
+    '2 Weeks' => '2 weken',
+    '<b>Note:</b> it may be neccesary to <b>hit F5</b> in editing pages in order to apply the new settings' => '<b>Let op:</b> mogelijk dient u <b>F5</b> in te drukken om de nieuwe instellingen toe te passen',
+    'Ace - plugin settings initialized.' => 'Ace - plugin settings initialized.',
+    'Ace - settings saved!' => 'Ace - instellingen opgeslagen!',
+    'Ace - unable to save ace_config.js file. Check permissions!' => 'Ace - het bestand ace_config.js kon niet worden opgeslagen. Controleer de permissies!',
+    'Ace - unable to store plugin settings!' => 'Ace - de plugin instellingen konden niet worden opgeslagen!',
+    'Ace - unable to store settings in database!' => 'Ace - de instellingen konden niet in de database worden opgeslagen!',
+    'Ace settings' => 'Ace instellingen',
+    'Code editor and syntax highlighter based on Ajax.org Cloud9 Editor.' => 'Code editor en syntax highlighter zijn gebaseerd op de Ajax.org Cloud9 Editor.',
+    'Cookies' => 'Cookies',
+    'Could not change ace_config.js file permissions to 0777. Do it manually!' => 'Kon de bestandspermissies van ace_config.js niet omzetten naar 0777. Doe dit handmatig!',
+    'Default editor box height in pixels' => 'Standaardhoogte van het bewerkvenster in pixels',
+    'Documentation' => 'Documentatie',
+    'Editor font size in pixels.' => 'Lettergrootte in pixels.',
+    'Editor height' => 'Hoogte bewerkvenster',
+    'Font size' => 'Lettergrootte',
+    'Highlight active line' => 'Markeer de huidige regel',
+    'How fast should your mousewheel scroll the editor' => 'Stel de gevoeligheid van het muiswiel in',
+    'How long to store scroll and mode settings?' => 'Hoe lang moeten de instellingen met betrekking tot de scrollsnelheid en bewerkmodus worden bewaard?',
+    'Individual settings for page parts, layouts and snippets - scroll position and syntax mode will be stored in cookie of your browser. Choose the default lifetime of such cookie.' => 'Individuele instellingen voor pagina delen, layouts en snippers, zoals de huidige positie en de bewerkmodus worden met een cookie opgeslagen in de browser. Kies hier hoe lang deze cookies standaard bewaard worden.',
+    'Integrate Ace with Layout editing?' => 'Integreer Ace met layoutbewerking?',
+    'Integration' => 'Integratie',
+    'Internal pages listing' => '',
+    'Look and feel' => 'Uiterlijk',
+    'Mode' => 'Modus',
+    'Mouse scroll speed' => 'Scrollsnelheid muiswiel',
+    'No' => 'Nee',
+    'Note that Ace won`t take precedence of other plugins being integrated with Layout (eg. Codemirror).' => 'Houd in uw achterhoofd dat Ace andere plugins die worden geïntegreerd met layoutbewerking (zoals Codemirror) niet overruled.',
+    'Plugin' => 'Plugin',
+    'Save' => 'Opslaan',
+    'Select default syntax highlighting mode' => 'Selecteer de standaard syntax markeer modus',
+    'Session-long' => 'Sessielengte',
+    'Settings' => 'Instellingen',
+    'Successfully uninstalled plugin.' => 'Plugin succesvol gedeïnstalleerd.',
+    'Theme' => 'Thema',
+    'Unable to delete plugin settings.' => 'Plugin instellingen konden niet worden verwijderd.',
+    'Wrap lines' => 'Automatische terugloop',
+    'Wrap long lines' => '',
+    'Yes' => 'Ja',
+    'You can obtain other themes at.' => '',
+    'unable to retrieve plugin settings.' => 'plugin instellingen konden niet worden opgehaald.',
+);
---
+++ i18n/template-message.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Wolf CMS - Content Management Simplified. <http://www.wolfcms.org>
+ * Copyright (C) 2008-2010 Martijn van der Kleijn <martijn.niji@gmail.com>
+ * 
+ * Ace filter for Wolf CMS
+ * Code editor and syntax highlighter based on Ajax.org Cloud9 Editor.
+ *  
+ * @package Plugins
+ * @subpackage ace
+ *
+ * @author Marek Murawski <http://marekmurawski.pl>
+ * @copyright Marek Murawski, 2012
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3 license
+ * @license Ace http://opensource.org/licenses/BSD-3-Clause BSD
+ * 
+ */
+
+/* Security measure */
+if (!defined('IN_CMS')) { exit(); }
+
+return array(
+    '1 Day' => '',
+    '1 Month' => '',
+    '1 Week' => '',
+    '1 Year' => '',
+    '10 Years' => '',
+    '2 Days' => '',
+    '2 Weeks' => '',
+    '<b>Note:</b> it may be neccesary to <b>hit F5</b> in editing pages in order to apply the new settings' => '',
+    'Ace - plugin settings initialized.' => '',
+    'Ace - settings saved!' => '',
+    'Ace - unable to save ace_config.js file. Check permissions!' => '',
+    'Ace - unable to store plugin settings!' => '',
+    'Ace - unable to store settings in database!' => '',
+    'Ace settings' => '',
+    'Code editor and syntax highlighter based on Ajax.org Cloud9 Editor.' => '',
+    'Cookies' => '',
+    'Could not change ace_config.js file permissions to 0777. Do it manually!' => '',
+    'Default editor box height in pixels' => '',
+    'Documentation' => '',
+    'Editor font size in pixels.' => '',
+    'Editor height' => '',
+    'Font size' => '',
+    'Highlight active line' => '',
+    'How fast should your mousewheel scroll the editor' => '',
+    'How long to store scroll and mode settings?' => '',
+    'Individual settings for page parts, layouts and snippets - scroll position and syntax mode will be stored in cookie of your browser. Choose the default lifetime of such cookie.' => '',
+    'Integrate Ace with Layout editing?' => '',
+    'Integration' => '',
+    'Internal pages listing' => '',
+    'Look and feel' => '',
+    'Mode' => '',
+    'Mouse scroll speed' => '',
+    'No' => '',
+    'Note that Ace won`t take precedence of other plugins being integrated with Layout (eg. Codemirror).' => '',
+    'Plugin' => '',
+    'Save' => '',
+    'Select default syntax highlighting mode' => '',
+    'Session-long' => '',
+    'Settings' => '',
+    'Successfully uninstalled plugin.' => '',
+    'Theme' => '',
+    'Unable to delete plugin settings.' => '',
+    'Wrap lines' => '',
+    'Wrap long lines' => '',
+    'Yes' => '',
+    'You can obtain other themes at.' => '',
+    'unable to retrieve plugin settings.' => '',
+);
