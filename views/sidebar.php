7ffeb724d3ac2e2e4be3d5805996dbb8aabc54ca
marekmurawski <marek.murawski@gmail.com>
2013-03-20 21:31:20 +0100

    Ace build update to 2013.03.14

--- views/sidebar.php
+++ views/sidebar.php
@@ -25,6 +25,6 @@ if ( !defined('IN_CMS') ) {
     <h2><?php echo Plugin::$plugins_infos['ace']->title; ?></h2>
     <p>
 <?php echo __('Plugin') . ': <b>' . Plugin::$plugins_infos['ace']->version ?></b><br/>
-        Ace: <b>0.2.0</b> <em>(2011.08.02)</em>
+        Ace: <b>0.2.0</b> <em>(build 2013.03.14)</em>
     </p>
 </div>
