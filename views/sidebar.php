894e9d671512fa4d863dccb56c5e3cfd6bf68cdf
marekmurawski <marek.murawski@gmail.com>
2013-03-21 16:01:21 +0100

    docs

--- views/sidebar.php
+++ views/sidebar.php
@@ -24,7 +24,8 @@ if ( !defined('IN_CMS') ) {
 <div class="box">
     <h2><?php echo Plugin::$plugins_infos['ace']->title; ?></h2>
     <p>
-<?php echo __('Plugin') . ': <b>' . Plugin::$plugins_infos['ace']->version ?></b><br/>
+        <?php echo __('Plugin') . ': <b>' . Plugin::$plugins_infos['ace']->version ?></b><br/>
         Ace: <b>0.2.0</b> <em>(build 2013.03.14)</em>
     </p>
 </div>
+<div style="clear: both; text-align: right;"><small><p>&copy; <a href="http://marekmurawski.pl" target="_blank" />Marek Murawski</a></p> </small></div>
