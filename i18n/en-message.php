73c4ee41076c2b765c1e88bb1afb4ec6becd24ff
marekmurawski <marek.murawski@gmail.com>
2013-04-15 08:35:06 +0200

    update i18n files for Wolf 0.7.6 version

---
+++ i18n/en-message.php
@@ -0,0 +1,66 @@
+<?php
+
+    /**
+     * YourLanguage file for plugin ace
+     *
+     * @package Plugins
+     * @subpackage ace
+     *
+     * @author Your Name <email@domain.something>
+     * @version Wolf x.y.z
+     */
+
+    return array(
+    	'1 Day' => '1 Day',
+	'1 Month' => '1 Month',
+	'1 Week' => '1 Week',
+	'1 Year' => '1 Year',
+	'10 Years' => '10 Years',
+	'2 Days' => '2 Days',
+	'2 Weeks' => '2 Weeks',
+	'Ace - plugin settings initialized.' => 'Ace - plugin settings initialized.',
+	'Ace - settings saved!' => 'Ace - settings saved!',
+	'Ace - unable to store plugin settings!' => 'Ace - unable to store plugin settings!',
+	'Ace - unable to store settings in database!' => 'Ace - unable to store settings in database!',
+	'Ace settings' => 'Ace settings',
+	'Code editor and syntax highlighter based on Ajax.org Cloud9 Editor.' => 'Code editor and syntax highlighter based on Ajax.org Cloud9 Editor.',
+	'Cookies' => 'Cookies',
+	'Default editor box height in pixels' => 'Default editor box height in pixels',
+	'Documentation' => 'Documentation',
+	'Editor font size in pixels.' => 'Editor font size in pixels.',
+	'Editor height' => 'Editor height',
+	'Font size' => 'Font size',
+	'Highlight active line' => 'Highlight active line',
+	'How fast should your mousewheel scroll the editor' => 'How fast should your mousewheel scroll the editor',
+	'How long to store scroll and mode settings?' => 'How long to store scroll and mode settings?',
+	'Individual settings for page parts, layouts and snippets - scroll position and syntax mode will be stored in cookie of your browser. Choose the default lifetime of such cookie.' => 'Individual settings for page parts, layouts and snippets - scroll position and syntax mode will be stored in cookie of your browser. Choose the default lifetime of such cookie.',
+	'Integrate Ace with Layout editing?' => 'Integrate Ace with Layout editing?',
+	'Integration' => 'Integration',
+	'Internal pages listing' => 'Internal pages listing',
+	'Look and feel' => 'Look and feel',
+	'Mode' => 'Mode',
+	'Mouse scroll speed' => 'Mouse scroll speed',
+	'No' => 'No',
+	'Note that Ace won`t take precedence of other plugins being integrated with Layout (eg. Codemirror).' => 'Note that Ace won`t take precedence of other plugins being integrated with Layout (eg. Codemirror).',
+	'Plugin' => 'Plugin',
+	'Save' => 'Save',
+	'Select default syntax highlighting mode' => 'Select default syntax highlighting mode',
+	'Session-long' => 'Session-long',
+	'Settings' => 'Settings',
+	'Successfully uninstalled plugin.' => 'Successfully uninstalled plugin.',
+	'Syntax' => 'Syntax',
+	'Theme' => 'Theme',
+	'Unable to delete plugin settings.' => 'Unable to delete plugin settings.',
+	'Wrap lines' => 'Wrap lines',
+	'Wrap long lines' => 'Wrap long lines',
+	'Wrapping' => 'Wrapping',
+	'Yes' => 'Yes',
+	'columns' => 'columns',
+	'don`t wrap' => 'don`t wrap',
+	'lines' => 'lines',
+	'margin' => 'margin',
+	'px' => 'px',
+	'to editor' => 'to editor',
+	'to margin' => 'to margin',
+	'unable to retrieve plugin settings.' => 'unable to retrieve plugin settings.',
+    );
